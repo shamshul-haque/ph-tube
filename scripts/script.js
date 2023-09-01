88b962fd7529abbef58ea7a0b3ac158f05c6b888
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-01 11:58:43 +0600

    showed the loaded category

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,22 @@
-// loading categories
+// loading all categories
 const loadCategories = async () => {
   const res = await fetch(
     "https://openapi.programming-hero.com/api/videos/categories"
   );
   const data = await res.json();
   const categories = data.data;
-  handleCategories(categories);
-};
-
-// handling the loaded categories
-const handleCategories = (categories) => {
-  console.log(categories);
-  categories.forEach((category) => {
-    category.categories;
-    console.log(category);
-  });
+  displayCategories(categories);
 };
 loadCategories();
+
+// showing the loaded categories
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("category-container");
+  categories.forEach((category) => {
+    const categoryItem = document.createElement("div");
+    categoryItem.innerHTML = `
+        <button onclick="loadSingleCategory('${category.category_id}')" class="bg-neutral-300 px-4 py-2 rounded-md text-base font-medium text-gray-600">${category.category}</button>
+    `;
+    categoryContainer.appendChild(categoryItem);
+  });
+};
