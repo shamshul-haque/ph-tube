c81bf4b340fb87a358b5126f0310d1d6b7c6135f
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-01 10:51:32 +0600

    category loaded

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log("connected");
+// loading categories
+const loadCategories = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const categories = data.data;
+  handleCategories(categories);
+};
+
+// handling the loaded categories
+const handleCategories = (categories) => {
+  console.log(categories);
+  categories.forEach((category) => {
+    category.categories;
+    console.log(category);
+  });
+};
+loadCategories();
